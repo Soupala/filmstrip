43e6c5fc34b6d325de187d60067e0dc9701015b7
Mica Cardillo <mica.cardillo@gmail.com>
2015-01-17 19:50:16 +0000

    adapting to Meteor video project

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'mica:filmstrip-reactive',
+  summary: 'Adapting HTML5 video filmstrip library to reactive video projects.',
+  version: '0.0.1',
+  git: 'https://github.com/Soupala/filmstrip.git'
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom('1.0.2.1');
+	api.addFiles(['filmstrip.js']);
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('mica:filmstrip-reactive');
+});
